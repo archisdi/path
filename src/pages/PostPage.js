a5be6bc1c97a14ad6a7876a372aac7edbf45aad0
Archie <archie.isdiningrat@gmail.com>
2020-09-24 16:54:45 +0700

    add infinite scroll

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,28 +1,31 @@
-import { Row } from "antd";
+import { Row, Spin } from "antd";
 import * as moment from "moment";
 import React, { useEffect, useState } from "react";
 import Post from "../components/Post";
 import PlacholderPost from "../components/Placeholder";
+import InfiniteScroll from "react-infinite-scroller";
+
+const INCREMENT = 10;
 
 const PostPage = (props) => {
   const [postProp, setPostProp] = useState([]);
+  const [limitData, setLimitData] = useState(5);
 
-  const withEx =
-    new URLSearchParams(props.location.search).get("ex") === "true";
-
-  useEffect(() => {
-    fetch(`${process.env.REACT_APP_RESOURCE_URI}.json`)
+  const loadData = (limit = INCREMENT) => {
+    fetch(
+      `${process.env.REACT_APP_RESOURCE_URI}.json?orderBy="date"&limitToLast=${limit}`
+    )
       .then((response) => response.json())
       .then((data) => {
-        const payload = Object.keys(data)
-          .map((item) => ({
-            ...data[item],
-            ex: withEx ? false : data[item].ex,
-          }))
-          .sort((a, b) => b.date - a.date);
-        setPostProp(payload);
+        setPostProp(data.filter((item) => item));
+        setLimitData(limit + INCREMENT);
       });
-  }, [withEx]);
+  };
+
+  /** on page load */
+  useEffect(() => {
+    loadData();
+  }, []);
 
   const posts = postProp.map((post, index) => {
     const date = moment.unix(post.date).format("DD MMM YYYY HH:mm");
@@ -43,8 +46,18 @@ const PostPage = (props) => {
     );
   });
 
+  const Posts = (
+    <InfiniteScroll
+      loadMore={() => loadData(limitData)}
+      hasMore={posts.length === limitData - INCREMENT}
+      loader={<Spin />}
+    >
+      {posts}
+    </InfiniteScroll>
+  );
+
   return (
-    <Row>{posts.length ? posts : <PlacholderPost count={6} rows={4} />}</Row>
+    <Row>{posts.length ? Posts : <PlacholderPost count={6} rows={4} />}</Row>
   );
 };
 
