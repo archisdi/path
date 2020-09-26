89c3b8ac28b6824ced3e4982f7cac7910ce59c70
Archie <archie.isdiningrat@gmail.com>
2020-09-26 11:18:57 +0700

    fix PWA installability

--- src/App.js
+++ src/App.js
@@ -1,41 +1,36 @@
 import { Layout } from "antd";
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import PostPage from "./pages/PostPage";
-import Logo from "./image/path-outline.png";
+import Logo from "./images/path-outline.png";
 
 const { Header, Content } = Layout;
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Layout>
-          <Header className="App-header">
-            <div className="logo">
-              <img
-                style={{ paddingBottom: 5 }}
-                width="75"
-                alt="path"
-                src={Logo}
-              ></img>
-            </div>
-          </Header>
-          <Content
-            style={{
-              padding: 24,
-              margin: 0,
-              minHeight: 280,
-            }}
-          >
-            <Switch>
-              <Route path="/" component={PostPage}></Route>
-            </Switch>
-          </Content>
-        </Layout>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <Layout>
+        <Header className="App-header">
+          <div className="logo">
+            <img
+              style={{ paddingBottom: 5 }}
+              width="75"
+              alt="path"
+              src={Logo}
+            ></img>
+          </div>
+        </Header>
+        <Content
+          style={{
+            padding: 24,
+            margin: 0,
+            minHeight: 280,
+          }}
+        >
+          <PostPage />
+        </Content>
+      </Layout>
+    </div>
   );
 };
 
